dadea4ff4625a68e1a4d1cd68781b08e6bae822f
PiQuark6046 <piquark6046@proton.me>
2023-08-31 16:47:41 +0000

    fix: delete docusaurus-theme-search-typesense and dotenv

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,6 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-require('dotenv').config();
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
@@ -50,7 +49,6 @@ const config = {
       }),
     ],
   ],
-  themes: ['docusaurus-theme-search-typesense'],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
@@ -151,19 +149,6 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-      },
-      typesense: {
-        typesenseCollectionName: 'list-kr-community',
-        typesenseServerConfig: {
-          nodes: [
-            {
-              host: process.env.TYPESENSE_HOST,
-              port: process.env.TYPESENSE_PORT,
-              protocol: process.env.TYPESENSE_PROTOCOL
-            }
-          ],
-          apiKey: process.env.TYPESENSE_API_KEY
-        }
       }
     })
 };
