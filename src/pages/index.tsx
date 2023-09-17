9aa611c38f6b7159d402f7dcc35f2ae58f68f3cc
piquark6046 <piquark6046@proton.me>
2023-09-17 17:06:26 +0000

    add: sitemap.xml and google verification

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import { Redirect } from '@docusaurus/router';
+import Layout from '@theme/Layout';
 
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Redirect to='/docs/'/>
+    <Layout>
+      <head>
+        <meta name="google-site-verification" content="GsgoCP3kPJHC9R9qlY-Zeri-K7Mg8wdMW1R8PgSxnKg" />
+      </head>
+      <Redirect to='/docs/'/>
+    </Layout>
   );
 }
