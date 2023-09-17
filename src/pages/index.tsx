a8683079ecd34831da7a1187f892822b40af76dc
piquark6046 <piquark6046@proton.me>
2023-09-17 17:23:51 +0000

    add: bing verification

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ export default function Home(): JSX.Element {
     <Layout>
       <Head>
         <meta name="google-site-verification" content="GsgoCP3kPJHC9R9qlY-Zeri-K7Mg8wdMW1R8PgSxnKg" />
+        <meta name="msvalidate.01" content="03372A5D8B91E88EB69C3E91197377B8" />
       </Head>
       <Redirect to='/docs/'/>
     </Layout>
