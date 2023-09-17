10016bc61f9be4e5906553db32a9d95e863f8da2
piquark6046 <piquark6046@proton.me>
2023-09-17 17:12:46 +0000

    fix: google verification

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import { Redirect } from '@docusaurus/router';
 import Layout from '@theme/Layout';
+import Head from '@docusaurus/Head';
 
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout>
-      <head>
+      <Head>
         <meta name="google-site-verification" content="GsgoCP3kPJHC9R9qlY-Zeri-K7Mg8wdMW1R8PgSxnKg" />
-      </head>
+      </Head>
       <Redirect to='/docs/'/>
     </Layout>
   );
