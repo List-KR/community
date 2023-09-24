770e75b7884af54c9449affce60f0b2969ad3c92
piquark6046 <piquark6046@proton.me>
2023-09-24 17:29:32 +0000

    add: naver verification

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ export default function Home(): JSX.Element {
       <Head>
         <meta name="google-site-verification" content="GsgoCP3kPJHC9R9qlY-Zeri-K7Mg8wdMW1R8PgSxnKg" />
         <meta name="msvalidate.01" content="03372A5D8B91E88EB69C3E91197377B8" />
+        <meta name="naver-site-verification" content="5294a2066a2b2c69bd4a4950288f1a23842fc67f" />
       </Head>
       <Redirect to='/docs/'/>
     </Layout>
