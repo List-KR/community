704c3e6ff34596b94a0b330ceef37123e68d9f80
piquark6046 <piquark6046@proton.me>
2023-09-24 17:01:42 +0000

    feat: update website domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   favicon: 'list-kr.jpeg',
 
   // Set the production url of your site here
-  url: 'https://list-kr-community.pages.dev',
+  url: 'https://community.list-kr.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
