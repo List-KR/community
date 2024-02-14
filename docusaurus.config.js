694d67006a2095085f546feb1f399bc73b401740
piquark6046 <piquark6046@proton.me>
2024-02-14 17:15:49 +0000

    update dependencies

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,8 +9,8 @@ fs.readdirSync('.').forEach(file => {
   }
 })
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require('prism-react-renderer').themes.github;
+const darkCodeTheme = require('prism-react-renderer').themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
